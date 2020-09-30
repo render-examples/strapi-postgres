d0f11541a2ff446aec4df83899fbff498bbdeeb6
David Mauskop <david@render.com>
2020-09-30 13:29:11 -0700

    Initial commit
    Generated with `yarn create strapi-app strapi-postgres --quickstart`

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e90f50793cca33e2f11fb21ffbaa735f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'
+};
