fc2393a4f9ba20985e4da9f5a538f06ecd6ac77f
Chris Castle <chris@crc.io>
2022-03-25 11:45:44 -0700

    Upgrade to Strapi v4 (#9)

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e90f50793cca33e2f11fb21ffbaa735f'),
-    },
+  app: {
+    keys: env.array('APP_KEYS')
   },
 });
