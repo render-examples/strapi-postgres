b0fb61c91470403a359ffaa9b5392ecbae719199
candidosales <candidosg@gmail.com>
2022-08-03 01:27:50 -0300

    chore: added apiToken.salt at admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
+    secret: env("ADMIN_JWT_SECRET", "b4dbad35-6caa-4f84-be56-90d0d3bc2fdf"),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "NR4Lk9xI80TAMm30Z8hd4OFsOZrn4m0u"),
   },
 });
