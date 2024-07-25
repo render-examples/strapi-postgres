38acb599588966cd5eb219a1c9bb4c9a1f82810b
WoWItsVhys <168016410+WoWItsVhys@users.noreply.github.com>
2024-07-25 18:56:42 -0400

    remove featured streamers

--- src/api/featured-streamer/controllers/featured-streamer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * featured-streamer controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::featured-streamer.featured-streamer');
--- src/api/featured-streamer/routes/featured-streamer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * featured-streamer router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::featured-streamer.featured-streamer');
