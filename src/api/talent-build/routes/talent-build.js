51a0c54e09e0f86674d7d1ac3aeb4d59c801615d
WoWItsVhys <168016410+WoWItsVhys@users.noreply.github.com>
2024-07-25 18:58:06 -0400

    add talent builds
    include many-to-one relation for spec and dungeon guides

---
+++ src/api/talent-build/routes/talent-build.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * talent-build router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::talent-build.talent-build');
---
+++ src/api/talent-build/services/talent-build.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * talent-build service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::talent-build.talent-build');
