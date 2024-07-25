b23f33091176b419977906a34b426e74c22d7ca9
WoWItsVhys <168016410+WoWItsVhys@users.noreply.github.com>
2024-07-25 18:56:42 -0400

    remove featured streamers

--- src/api/featured-streamer/services/featured-streamer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * featured-streamer service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::featured-streamer.featured-streamer');
